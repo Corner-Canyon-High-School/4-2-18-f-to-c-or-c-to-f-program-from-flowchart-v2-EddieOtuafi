4b5a34594cc4eac383f6d19dec79b15c680d6e28
EddieOtuafi <162729686+EddieOtuafi@users.noreply.github.com>
2024-04-17 16:39:27 +0000

    submit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+temp=int(input("Enter the temperature: "))
+choice=input("type 1 for fahrenheit to centigrade and type 2 for centigrade to fahrenheit: ")
+f = (9*temp + (32 * 5))/5
+c = (5/9) * (temp - 32)
+if choice == "1":
+    print(str(temp)+"c")
+elif choice == "2":
+    print(str(temp)+"f")
+else:
+    print("Invalid Input")
